bb24413cad122d6203d955b1a58477ebdc720dce
Leo Araujo <leonardo_02452@hotmail.com>
2022-12-16 17:20:27 -0300

    feat: add get_available_dishes method to InventoryControl class

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -20,13 +20,33 @@ class InventoryControl:
 
     def add_new_order(self, customer, order, day):
         for ingredient in self.INGREDIENTS[order]:
+            if self._inventory[ingredient] <= 0:
+                return False
             self._inventory[ingredient] -= 1
 
     def get_quantities_to_buy(self):
         quantities_to_buy = dict()
 
         for ingredient in self.MINIMUM_INVENTORY:
-            amount_difference = self.MINIMUM_INVENTORY[ingredient] - self._inventory[ingredient]
+            minimun_ingredient_amount = self.MINIMUM_INVENTORY[ingredient]
+            actual_ingredient_amount = self._inventory[ingredient]
+            amount_difference = (
+                minimun_ingredient_amount - actual_ingredient_amount
+            )
             quantities_to_buy[ingredient] = amount_difference
-            
+
         return quantities_to_buy
+
+    def get_available_dishes(self):
+        available_ingredients = set()
+        available_dishes = set()
+
+        for ingredient in self._inventory:
+            if self._inventory[ingredient] > 0:
+                available_ingredients.add(ingredient)
+
+        for dish, ingredients in self.INGREDIENTS.items():
+            if set(ingredients).issubset(available_ingredients):
+                available_dishes.add(dish)
+
+        return available_dishes
